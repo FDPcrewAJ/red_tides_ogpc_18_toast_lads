eb1ecba55a8517137b75c25db7a8e4f2d093730a
Jungels_Aiden <junga526@hsd.k12.or.us>
2024-12-02 16:02:16 -0800

    prefixed argument parameters with _ to remove debugger warning

--- gd_root/Scripts/Misc/dev_testing_room.gd
+++ gd_root/Scripts/Misc/dev_testing_room.gd
@@ -7,9 +7,9 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta) -> void:
 	pass
 
 
-func _on_area_3d_area_entered(area: Area3D) -> void:
+func _on_area_3d_area_entered(_area) -> void:
 	get_tree().change_scene_to_file("res://Scenes/Testing Rooms/2d_dev_testing_room.tscn")

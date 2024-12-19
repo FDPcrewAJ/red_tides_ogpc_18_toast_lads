c3c02b3e113d6d85b0760281c221edc4eedba595
Ainsworth_Adam <ainsa128@hsd.k12.or.us>
2024-12-18 16:57:28 -0800

    fixed a bug that caused the storm timer to reset when entering a room at 0

--- gd_root/Scripts/Rooms/RoomObjects/storm.gd
+++ gd_root/Scripts/Rooms/RoomObjects/storm.gd
@@ -1,7 +1,7 @@
 extends Timer
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
 	if Global.timer_started == true:
 		$".".wait_time = Global.storm_time
 	Global.timer_started = true
@@ -9,9 +9,9 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+func _process(delta: float) -> void:
 	Global.storm_time = int(time_left)
 
 
-func _on_timeout():
+func _on_timeout() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Menus/main_menu.tscn")

eedfe71e8114fbdff16e88e781e69e230aa4fd0c
Jungels_Aiden <junga526@hsd.k12.or.us>
2024-12-18 16:11:39 -0800

    fixed most of the call_deffered and unused parameter warnings

--- gd_root/Scripts/Rooms/RoomManagers/reactor.gd
+++ gd_root/Scripts/Rooms/RoomManagers/reactor.gd
@@ -1,15 +1,4 @@
 extends Node3D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-func _on_hub_portal_area_entered(area: Area3D) -> void:
+func _on_hub_portal_area_entered(_area):
 	get_tree().change_scene_to_file("res://Scenes/testingLevels/hub.tscn")
--- gd_root/Scripts/Rooms/RoomObjects/storm.gd
+++ gd_root/Scripts/Rooms/RoomObjects/storm.gd
@@ -1,7 +1,7 @@
 extends Timer
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _ready():
 	if Global.timer_started == true:
 		$".".wait_time = Global.storm_time
 	Global.timer_started = true
@@ -9,9 +9,9 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta):
 	Global.storm_time = int(time_left)
 
 
-func _on_timeout() -> void:
+func _on_timeout():
 	get_tree().change_scene_to_file("res://Scenes/Menus/main_menu.tscn")

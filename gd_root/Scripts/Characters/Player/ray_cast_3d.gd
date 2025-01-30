596af01daff72de70792e61601cc88c9922c54ae
Jungels_Aiden <junga526@hsd.k12.or.us>
2025-01-29 16:56:42 -0800

    started working on the main floor

--- gd_root/Scripts/Characters/Player/ray_cast_3d.gd
+++ gd_root/Scripts/Characters/Player/ray_cast_3d.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if is_colliding() == true:
 		cur_object = get_collider().get_name()
 		cur_object = str(cur_object)

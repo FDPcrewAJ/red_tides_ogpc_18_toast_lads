82e89b7f53df79686b070aad6ec64b44485759e5
Your Name <you@example.com>
2025-02-14 14:39:39 -0800

    updated outside rig model to fit concept

--- gd_root/Scripts/Rooms/RoomObjects/boat.gd
+++ gd_root/Scripts/Rooms/RoomObjects/boat.gd
@@ -8,5 +8,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if self.position.x > -62:
+	if self.position.x > -71:
 		translate(Vector3(0,0,0.1 * 1))

a8b2bec1b958de3125e6cb7d3b2d6daa094ae954
FDPcrewAJ <junga526@hsd.k12.or.us>
2025-03-09 16:17:47 -0700

    updated input map name on dial puzzle script

--- gd_root/Scripts/Rooms/RoomObjects/dial_computer.gd
+++ gd_root/Scripts/Rooms/RoomObjects/dial_computer.gd
@@ -6,6 +6,6 @@ func _ready():
 
 func _process(_delta):
 	if Global.current_object == "ConsoleBody":
-		if Input.is_action_just_pressed("click"):
+		if Input.is_action_just_pressed("interact"):
 			Global.lastpos = $"../player".position
 			get_tree().call_deferred("change_scene_to_file", "res://Scenes/Interfaces/dial_puzzle_interface.tscn")

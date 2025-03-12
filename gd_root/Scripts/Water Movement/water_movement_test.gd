d23ef2799f9dfd161311655ee9dfe5e185deae87
Jungels_Aiden <junga526@hsd.k12.or.us>
2025-03-12 16:45:16 -0700

    worked on facility exterior

--- gd_root/Scripts/Water Movement/water_movement_test.gd
+++
@@ -1,9 +0,0 @@
-extends Node3D
-
-@onready var path = $world/Path3D/PathFollow3D
-@onready var second_path = $world/Path3D/PathFollow3D/Path3D/PathFollow3D
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	path.progress += 0.2
-	second_path.progress += 0.01

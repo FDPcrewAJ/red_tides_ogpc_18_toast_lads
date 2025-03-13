24f66a94fa09ba007360de221554cbb3e24c28a2
Aiden <junga526@hsd.k12.or.us>
2025-03-12 23:05:50 -0700

    created elevator, first floor, and underwater floor scenes

--- gd_root/Scripts/Menus/level_select_menu.gd
+++ gd_root/Scripts/Menus/level_select_menu.gd
@@ -25,3 +25,11 @@ func _on_pipe_puzzle_pressed() -> void:
 
 func _on_dial_puzzle_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/testingLevels/dial_puzzle.tscn")
+
+
+func _on_facility_exterior_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Final Levels/facility_exterior.tscn")
+
+
+func _on_underwater_floor_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Final Levels/underwater_floor.tscn")

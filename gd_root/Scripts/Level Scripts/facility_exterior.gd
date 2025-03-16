b7520fcd8d712fcd8cac645d5aaef127fa63025d
FDPcrewAJ <junga526@hsd.k12.or.us>
2025-03-16 13:57:14 -0700

    added water death hitbox and fixed pause menu

--- gd_root/Scripts/Level Scripts/facility_exterior.gd
+++ gd_root/Scripts/Level Scripts/facility_exterior.gd
@@ -1,8 +1,15 @@
 extends Node3D
 
+@onready var water_hitbox = $world/Water/waterCol
+
 @onready var boat = $world/boat
 var boat_end_pos = -44.0
 
 func _process(_delta):
 	if boat.position.z > boat_end_pos:
 		boat.translate(Vector3(0,0,-0.01 * 1))
+
+
+func _on_water_col_area_entered(_area):
+	get_tree().call_deferred("change_scene_to_file", "res://Scenes/Final Levels/facility_exterior.tscn")
+	

e4753f02c66cd91eeec04880ed90652009b4659d
FDPcrewAJ <junga526@hsd.k12.or.us>
2025-03-16 14:40:17 -0700

    updated facility exterior enviroment

--- gd_root/Scripts/Menus/settings_script.gd
+++ gd_root/Scripts/Menus/settings_script.gd
@@ -1,5 +1,8 @@
 extends Control
 
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
 
 func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file(Global.previous_scene)

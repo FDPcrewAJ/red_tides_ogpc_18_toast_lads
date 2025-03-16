0127d1b84b870f6ac028f3ce0835f4f4fc37f4c2
FDPcrewAJ <junga526@hsd.k12.or.us>
2025-03-16 14:34:21 -0700

    conected facility exterior door to pipe puzzle room

--- gd_root/Scripts/Level Scripts/facility_exterior.gd
+++ gd_root/Scripts/Level Scripts/facility_exterior.gd
@@ -1,13 +1,19 @@
 extends Node3D
 
 @onready var water_hitbox = $world/Water/waterCol
+@onready var player = $world/boat/player
 
 @onready var boat = $world/boat
 var boat_end_pos = -44.0
 
+
 func _process(_delta):
 	if boat.position.z > boat_end_pos:
 		boat.translate(Vector3(0,0,-0.01 * 1))
+	
+	if Global.current_object == "doorCol":
+		if Input.is_action_just_pressed("interact"):
+			get_tree().call_deferred("change_scene_to_file", "res://Scenes/Testing Rooms/pipe_puzzle_test.tscn")
 
 
 func _on_water_col_area_entered(_area):

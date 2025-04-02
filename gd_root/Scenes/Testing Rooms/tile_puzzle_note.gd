4d87ef474059ca338800719606544ba4e2b9978d
Ainsworth_Adam <ainsa128@hsd.k12.or.us>
2025-04-02 16:27:54 -0700

    added sticky notes explaining how puzzles work

--- gd_root/Scenes/Testing Rooms/tile_puzzle_note.gd
+++ gd_root/Scenes/Testing Rooms/tile_puzzle_note.gd
@@ -9,7 +9,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	#This needs to correspond to the note's body node
-	if Global.current_object == "DialPuzzleNoteBody":
+	if Global.current_object == "TilePuzzleNoteBody":
 		if Input.is_action_just_pressed("interact"):
 			#This needs to correspond to the menu prompt node in the current scene.
-			$"../MenuPrompt"._set_label_prompt_text("If this terminal is still in calibration mode, you just need to turn the dials to the corresponding angles. The angles you need to turn them to are written down... somewhere around this place.  \n \n -Engineering")
+			$"../MenuPrompt"._set_label_prompt_text("If this terminal is still in calibration mode, you need to input the correct tile pattern. There's a poster nearby that tells you what tiles you need to input depending on your level. It resets if you look away from it though, I'll probably fix that eventually. \n \n -Engineering")
--- gd_root/Scripts/Menus/menu_prompt.gd
+++ gd_root/Scripts/Menus/menu_prompt.gd
@@ -3,24 +3,34 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$PromptLabel.hide()
+	$CloseButton.hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
+func pause():
+	get_tree().paused = true
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+
+func unpause():
+	get_tree().paused = false
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
 func _set_label_prompt_text(new_text):
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	$PromptLabel._set_prompt_text(new_text)
-	$PromptLabel.visible = true
-	$CloseButton.visible = true
-	$CloseButton.disabled = false
-	get_tree().paused = true
+	$PromptLabel.show()
+	$CloseButton.show()
+	Global.in_menu = true
+	pause()
 
 func _on_close_button_pressed() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	$PromptLabel.visible = false
-	$CloseButton.visible = false
-	$CloseButton.disabled = true
-	get_tree().paused = false
+	$PromptLabel.hide()
+	$CloseButton.hide()
+	Global.in_menu = false
+	unpause()

5df142d2f64dff421608e731fc212a3aafe8a9d3
Ainsworth_Adam <ainsa128@hsd.k12.or.us>
2025-04-02 16:48:50 -0700

    Made text display when the player faces an interactable object

--- gd_root/Scenes/Testing Rooms/dial_puzzle_note.gd
+++ gd_root/Scenes/Testing Rooms/dial_puzzle_note.gd
@@ -10,6 +10,8 @@ func _ready() -> void:
 func _process(_delta):
 	#This needs to correspond to the note's body node
 	if Global.current_object == "DialPuzzleNoteBody":
+		#Must correspond to the interactables display node in the current scene
+		$"../InteractablesDisplay"._show_interactable()
 		if Input.is_action_just_pressed("interact"):
 			#This needs to correspond to the menu prompt node in the current scene.
 			$"../MenuPrompt"._set_label_prompt_text("If this terminal is still in calibration mode, you just need to turn the dials to the corresponding angles. The angles you need to turn them to are written down... somewhere around this place.  \n \n -Engineering")
--- gd_root/Scenes/testingLevels/fire_puzzle_note.gd
+++ gd_root/Scenes/testingLevels/fire_puzzle_note.gd
@@ -10,6 +10,8 @@ func _ready() -> void:
 func _process(_delta):
 	#This needs to correspond to the note's body node
 	if Global.current_object == "FirePuzzleNoteBody":
+		#Must correspond to the interactables display node in the current scene
+		$"../InteractablesDisplay"._show_interactable()
 		if Input.is_action_just_pressed("interact"):
 			#This needs to correspond to the menu prompt node in the current scene.
 			$"../MenuPrompt"._set_label_prompt_text("There are a lot of exposed wires around here, so incase things catch on fire, here's how to use the fire supression system. There are 3 terminals around the room, and each one is connected to a seperate water pipe. You can use a terminal to direct water to one of six areas, but each terminal can only access enough water to put out one fire at a time. You can change which area is being put out whenever you want though. Yeah, this system is so scuffed. Can't believe I even have to write this. \n \n -Engineering")

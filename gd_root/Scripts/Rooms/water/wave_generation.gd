98693e9d5a7af21450674700fdbf3924d42860f9
FDPcrewAJ <junga526@hsd.k12.or.us>
2025-04-02 15:28:49 -0700

    started working on ocean water generation

---
+++ gd_root/Scripts/Rooms/water/wave_generation.gd
@@ -0,0 +1,15 @@
+@tool
+extends StaticBody3D
+
+@export var gen_water = false
+
+@onready var water_mesh = $water_mesh
+
+func _physics_process(_delta):
+	if Input.is_action_just_pressed("jump"):
+		gen_water = true
+		generate_water()
+		gen_water = false
+
+func generate_water():
+	print("worked")

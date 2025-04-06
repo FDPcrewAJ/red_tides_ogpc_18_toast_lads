07877fe0c397447dc4b6ffd248116273f582eb58
FDPcrewAJ <junga526@hsd.k12.or.us>
2025-04-06 15:05:43 -0700

    updated water generation with export vars for easy control

--- gd_root/Scripts/Rooms/water/water_generator.gd
+++ gd_root/Scripts/Rooms/water/water_generator.gd
@@ -1,26 +1,36 @@
 extends MeshInstance3D
 
 # X and Z size of the mesh
+@export_group("Mesh Size")
 @export var xSize = 20
 @export var zSize = 20
-
-# Height and intenisty adjustments
-@export var terrain_height = 5
-@export var noise_intensity = 0.2
 @export var vert_distance = 5
 
+# Height and intenisty adjustments
+@export_group("Basic Perlin Adjustments")
+@export var terrain_height = 5
+@export var noise_intensity = 0.2
+
+# Time between new mesh creation/distance on perlin noise in that time
+@export_group("Time and Perlin Distance")
+@export var time = 0.0
+@export var speed = 0.2
+
+# Extra control over generation
+@export_group("Experimental Additional Generation Control")
+@export var jitter = 10
+@export var clamp_min = 0.0
+@export var clamp_max = 0.5
+
 # Control variables to update and reset changed meshes
+@export_group("Update Generation In Editor")
 @export var update = false
 @export var clear_vert_vis = false
-
-var time = 0.0
-var speed = 0.2
-
-var jitter = 10
 
 
 func _ready():
 	generate_terrain()
+	
 
 func generate_terrain():
 	var a_mesh:ArrayMesh
@@ -34,7 +44,7 @@ func generate_terrain():
 	
 	for z in range(zSize + 1):
 		for x in range(xSize + 1):
-			var y = clamp(noise.get_noise_2d((x * noise_intensity + time) / jitter, (z * noise_intensity + time) / jitter) * terrain_height, 0.0, 0.5)
+			var y = clamp(noise.get_noise_2d((x * noise_intensity + time) / jitter, (z * noise_intensity + time) / jitter) * terrain_height, clamp_min, clamp_max)
 			
 			var uv = Vector2()
 			uv.x = inverse_lerp(0, xSize, x)

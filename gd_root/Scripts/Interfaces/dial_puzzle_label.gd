dabe7d1240edcc74b616b753448ed24c857ea9c5
Ainsworth_Adam <ainsa128@hsd.k12.or.us>
2025-04-07 16:38:42 -0700

    fixed bugs with the dial puzzle

--- gd_root/Scripts/Interfaces/dial_puzzle_label.gd
+++ gd_root/Scripts/Interfaces/dial_puzzle_label.gd
@@ -2,7 +2,7 @@ extends Label
 
 
 func _process(_delta):
-	if ($"../Dial/Sprite2D1".rotation_degrees < -85 and $"../Dial/Sprite2D1".rotation_degrees > -95) and ($"../Dial2/Sprite2D2".rotation_degrees < -40 and $"../Dial2/Sprite2D2".rotation_degrees > -50) and ($"../Dial3/Sprite2D3".rotation_degrees < -175 and $"../Dial3/Sprite2D3".rotation_degrees > -185):
+	if ($"../Dial/Sprite2D1".rotation_degrees < -85 and $"../Dial/Sprite2D1".rotation_degrees > -95) and ($"../Dial2/Sprite2D2".rotation_degrees < -40 and $"../Dial2/Sprite2D2".rotation_degrees > -50) and ($"../Dial3/Sprite2D3".rotation_degrees < -175 and $"../Dial3/Sprite2D3".rotation_degrees > -185) or Global.dial_puzzle_completed == true:
 		text = "Calibration Completed"
 		Global.dial_puzzle_completed = true
 		Global.dial_puzzle = true

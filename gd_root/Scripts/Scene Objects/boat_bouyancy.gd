e74f8d1c55b39faa0e3d987e643d6bd0cbaee541
FDPcrewAJ <junga526@hsd.k12.or.us>
2025-04-08 22:24:47 -0700

    tried and failed to get boat floating to update with the shader, have zero clue what the issue is

--- gd_root/Scripts/Scene Objects/boat_bouyancy.gd
+++ gd_root/Scripts/Scene Objects/boat_bouyancy.gd
@@ -4,24 +4,22 @@ extends RigidBody3D
 @export var water_drag = 0.05
 @export var water_angular_drag = 0.05
 
-@export var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 @onready var water = get_parent().get_node("water_plane")
-
-const water_height := 0.0
+@onready var probes = $probe_container.get_children()
 
 var submerged = false
 
-
-
 func _physics_process(_delta):
 	submerged = false
-	var depth = water.get_height(global_position) - global_position.y
-	if depth > 0:
-		submerged = true
-		apply_central_force(Vector3.UP * float_force * gravity * depth)
+	for p in probes:
+		var depth = water.get_height(p.global_position) - p.global_position.y
+		if depth > 0:
+			submerged = true
+			apply_force(Vector3.UP * float_force * gravity * depth, p.global_position - global_position)
 
 func _integrate_forces(state: PhysicsDirectBodyState3D):
 	if submerged:
-		state.linear_velocity *= 1 - water_drag
-		state.angular_velocity *= 1 - water_angular_drag
+		state.linear_velocity *=  1 - water_drag
+		state.angular_velocity *= 1 - water_angular_drag 

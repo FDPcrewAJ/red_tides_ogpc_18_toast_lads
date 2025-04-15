f9b72c84a653898ad93b8310bef3ed0ec8de6e13
Jungels_Aiden <junga526@hsd.k12.or.us>
2025-04-15 12:48:51 -0700

    escape now works to exit pause and settings menus

--- gd_root/Scripts/Menus/settings_script.gd
+++ gd_root/Scripts/Menus/settings_script.gd
@@ -4,5 +4,9 @@ func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 
-func _on_back_button_pressed() -> void:
+func _on_back_button_pressed():
 	self.visible = false
+
+func _unhandled_input(_event):
+	if Input.is_action_just_released("exit"):
+		self.visible = false

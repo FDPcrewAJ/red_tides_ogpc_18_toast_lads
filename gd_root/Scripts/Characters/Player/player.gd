c4012a4f0ce39a271534281c9d3481df7a0ced2e
FDPcrewAJ <junga526@hsd.k12.or.us>
2025-04-17 17:32:49 -0700

    set up intro sequence frame work

--- gd_root/Scripts/Characters/Player/player.gd
+++ gd_root/Scripts/Characters/Player/player.gd
@@ -9,6 +9,9 @@ const mouse_sens = 0.5
 @onready var standing_collision = $StandingCollision
 @onready var crouching_collision = $CrouchingCollision
 
+# If player can move or not (intro/cutscene control)
+var has_control = true
+
 # Player Movement Variables
 var direction = Vector3.ZERO
 const move_lerp_speed = 7.0
@@ -16,7 +19,7 @@ var current_speed = 0.0
 var crouch_height = 0.4
 var stand_height = 0.8
 
-
+# State Machine Control
 var walking = false
 var sprinting = false
 var crouching = false
@@ -40,67 +43,68 @@ func _input(event):
 
 
 func _physics_process(delta):
-	# Add gravity
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-	
-	# Jump
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_velocity
-	
-	if Input.is_action_pressed("crouch"):
-		crouching = true
-	if Input.is_action_just_released("crouch"):
-		crouching = false
-	
-	if crouching:
-		standing_collision.disabled = true
-		crouching_collision.disabled = false
-		head.position.y = lerp(head.position.y, crouch_height, delta * move_lerp_speed)
-	elif !crouching && !collision_check.is_colliding():
-		standing_collision.disabled = false
-		crouching_collision.disabled = true
-		head.position.y = lerp(head.position.y, stand_height, delta * move_lerp_speed)
+	if has_control:
+		# Add gravity
+		if not is_on_floor():
+			velocity += get_gravity() * delta
+		
+		# Jump
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = jump_velocity
+		
+		if Input.is_action_pressed("crouch"):
+			crouching = true
+		if Input.is_action_just_released("crouch"):
+			crouching = false
+		
+		if crouching:
+			standing_collision.disabled = true
+			crouching_collision.disabled = false
+			head.position.y = lerp(head.position.y, crouch_height, delta * move_lerp_speed)
+		elif !crouching && !collision_check.is_colliding():
+			standing_collision.disabled = false
+			crouching_collision.disabled = true
+			head.position.y = lerp(head.position.y, stand_height, delta * move_lerp_speed)
 
-	## Get the input direction in which player is moving 
-	## (in Vector 2: 
-	## Left: (-1, 0)
-	## Right: (1, 0)
-	## Forward: (0, -1)
-	## Backward (0, 1)
-	## when moving diagonally, you get a fancy decimal 0.707 something)
-	var input_dir := Input.get_vector("left", "right", "forward", "backward")
-	
-	## Direction varible to store x and z direction that the player is moving
-	## (Is not the players actual velocity, that is different and gets put in later)
-	## Lerp: goes from first input to second input smoothly
-	## [Input 1]: Current direction of the player, changes as the player moves. (in Vector3)
-	## [Input 2]: Mouse position of the player (transform.basis), gets where the player is looking, 
-	## (in Vector3) so that we know what direction we need to move in. This is then multiplied by a
-	## Vector3 conversion of the input direction, with y being 0 as we are not moving vertical.
-	## All of that is then normalized, which sets the distance of the vector3 to one.
-	## [Input 3]: The rate of change is the set lerp speed, effects how slowly or quickly you speed 
-	## up, multiplied by delta so that it sticks with frame time and does not go out of sync.
-	direction = lerp(direction,
-					(transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(), 
-					delta * move_lerp_speed)
-	
-	if crouching:
-		current_speed = crouch_speed
-	elif Input.is_action_pressed("sprint"):
-		current_speed = sprint_speed
-	else:
-		current_speed = walk_speed
-	
-	if direction:
-		velocity.x = direction.x * current_speed
-		velocity.z = direction.z * current_speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, current_speed)
-		velocity.z = move_toward(velocity.z, 0, current_speed)
-	
-	
-	move_and_slide()
+		## Get the input direction in which player is moving 
+		## (in Vector 2: 
+		## Left: (-1, 0)
+		## Right: (1, 0)
+		## Forward: (0, -1)
+		## Backward (0, 1)
+		## when moving diagonally, you get a fancy decimal 0.707 something)
+		var input_dir := Input.get_vector("left", "right", "forward", "backward")
+		
+		## Direction varible to store x and z direction that the player is moving
+		## (Is not the players actual velocity, that is different and gets put in later)
+		## Lerp: goes from first input to second input smoothly
+		## [Input 1]: Current direction of the player, changes as the player moves. (in Vector3)
+		## [Input 2]: Mouse position of the player (transform.basis), gets where the player is looking, 
+		## (in Vector3) so that we know what direction we need to move in. This is then multiplied by a
+		## Vector3 conversion of the input direction, with y being 0 as we are not moving vertical.
+		## All of that is then normalized, which sets the distance of the vector3 to one.
+		## [Input 3]: The rate of change is the set lerp speed, effects how slowly or quickly you speed 
+		## up, multiplied by delta so that it sticks with frame time and does not go out of sync.
+		direction = lerp(direction,
+						(transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(), 
+						delta * move_lerp_speed)
+		
+		if crouching:
+			current_speed = crouch_speed
+		elif Input.is_action_pressed("sprint"):
+			current_speed = sprint_speed
+		else:
+			current_speed = walk_speed
+		
+		if direction:
+			velocity.x = direction.x * current_speed
+			velocity.z = direction.z * current_speed
+		else:
+			velocity.x = move_toward(velocity.x, 0, current_speed)
+			velocity.z = move_toward(velocity.z, 0, current_speed)
+		
+		
+		move_and_slide()
 
 
 func _set_last_pos():
--- gd_root/Scripts/Level Scripts/facility_exterior.gd
+++ gd_root/Scripts/Level Scripts/facility_exterior.gd
@@ -2,11 +2,14 @@ extends Node3D
 
 @onready var player = $player
 @onready var boat = $world/boat
+@onready var intro_camera: Camera3D = $intro_camera
+@onready var intro_objects: Node3D = $world/boat/intro_objects
 
 var boat_end_pos = -44.0
 
 
 func _ready():
+	player.has_control = false
 	boat.axis_lock_linear_x = true
 	if Global.consistent_positioning == true:
 		if typeof(Global.new_position) != TYPE_INT:
@@ -15,14 +18,21 @@ func _ready():
 			Global.consistent_positioning = false
 
 func _process(_delta):
+	#Room Switching
 	if Global.current_object == "doorCol":
 		if Input.is_action_just_pressed("interact") or Input.is_action_just_pressed("left_click"):
 			Global.floor = "Hub"
 			get_tree().call_deferred("change_scene_to_file", "res://Scenes/Testing Rooms/pipe_puzzle_test.tscn")
+	
+	# Boat Movement Control
 	if boat.global_position.z > boat_end_pos:
 		boat.apply_central_force(Vector3(0,0,-5))
 	else:
 		boat.axis_lock_linear_z = true
+		intro_objects.visible = false
+		intro_camera.current = false
+		player.visible = true
+		player.has_control = true
 
 
 func _on_death_col_area_entered(_area):

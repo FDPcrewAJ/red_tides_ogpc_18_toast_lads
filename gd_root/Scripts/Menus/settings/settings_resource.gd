355c105ca863634e283c73c106f8208af00e0b62
Jungels_Aiden <junga526@hsd.k12.or.us>
2025-04-18 09:47:50 -0700

    audio is no longer muted by default when no save file is present

--- gd_root/Scripts/Menus/settings/settings_resource.gd
+++ gd_root/Scripts/Menus/settings/settings_resource.gd
@@ -3,7 +3,7 @@ extends Resource
 
 const DEFAULT_WINDOW_MODE_INDEX : int = 0
 const DEFAULT_RESOLUTION_INDEX : int = 0
-const DEFAULT_MASTER_VOLUME : float = 0.0
-const DEFAULT_MUSIC_VOLUME : float = 0.0
-const DEFAULT_SFX_VOLUME : float = 0.0
-const DEFAULT_VOICE_VOLUME : float = 0.0
+const DEFAULT_MASTER_VOLUME : float = 1.0
+const DEFAULT_MUSIC_VOLUME : float = 1.0
+const DEFAULT_SFX_VOLUME : float = 1.0
+const DEFAULT_VOICE_VOLUME : float = 1.0

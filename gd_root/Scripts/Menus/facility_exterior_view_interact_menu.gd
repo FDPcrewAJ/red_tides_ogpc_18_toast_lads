94fe7aa5cf0d649c30d824d0d5269fe1afcb1877
Aiden <junga526@hsd.k12.or.us>
2025-04-17 19:11:45 -0700

    got facility exterior interact display working

---
+++ gd_root/Scripts/Menus/facility_exterior_view_interact_menu.gd
@@ -0,0 +1,8 @@
+extends Node3D
+
+@onready var interactables_display = $"../InteractablesDisplay"
+
+
+func _process(delta):
+	if Global.current_object == "doorCol":
+		interactables_display._show_interactable()

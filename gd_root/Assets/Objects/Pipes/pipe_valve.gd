d88be0fb000c22bdccac206232eb6c5eb881f508
Jungels_Aiden <junga526@hsd.k12.or.us>
2025-04-21 13:07:32 -0700

    added animations to the valves on underwater puzzle

--- gd_root/Assets/Objects/Pipes/pipe_valve.gd
+++ gd_root/Assets/Objects/Pipes/pipe_valve.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 @onready var interactables_display: Control = $"../../../InteractablesDisplay"
 @onready var reactor_label: Label3D = $"../../../reactor_main/central_column/MeshInstance3D5/ReactorLabel"
+@onready var valve_anims: AnimationPlayer = $"../../../valve_anims"
 
 var maxCoolant = 0
 var coolantProvided = 0
@@ -23,35 +24,43 @@ func _process(_delta) -> void:
 	if Global.current_object == "ValveBody":
 		#Must correspond to the interactables display node in the current scene
 		interactables_display._show_interactable()
-		if Input.is_action_just_pressed("interact") or Input.is_action_just_pressed("left_click"):
-			if coolantProvided < maxCoolant:
-				if self.name == "pipe_valve":
-					reactor_label.coolant += 10
-					coolantProvided += 10
+		if !valve_anims.is_playing():
+			if Input.is_action_just_pressed("interact") or Input.is_action_just_pressed("left_click"):
+				if coolantProvided < maxCoolant:
+					if self.name == "pipe_valve":
+						valve_anims.play("pipe_1_turn")
+						reactor_label.coolant += 10
+						coolantProvided += 10
 	if Global.current_object == "ValveBody2":
 		#Must correspond to the interactables display node in the current scene
 		interactables_display._show_interactable()
-		if Input.is_action_just_pressed("interact") or Input.is_action_just_pressed("left_click"):
-			if coolantProvided < maxCoolant:
-				if self.name == "pipe_valve2":
-					reactor_label.coolant += 15
-					coolantProvided += 15
+		if !valve_anims.is_playing():
+			if Input.is_action_just_pressed("interact") or Input.is_action_just_pressed("left_click"):
+				if coolantProvided < maxCoolant:
+					if self.name == "pipe_valve2":
+						valve_anims.play("pipe_2_turn")
+						reactor_label.coolant += 15
+						coolantProvided += 15
 	if Global.current_object == "ValveBody3":
 		#Must correspond to the interactables display node in the current scene
 		interactables_display._show_interactable()
-		if Input.is_action_just_pressed("interact") or Input.is_action_just_pressed("left_click"):
-			if coolantProvided < maxCoolant:
-				if self.name == "pipe_valve3":
-					reactor_label.coolant += 2
-					coolantProvided += 2
+		if !valve_anims.is_playing():
+			if Input.is_action_just_pressed("interact") or Input.is_action_just_pressed("left_click"):
+				if coolantProvided < maxCoolant:
+					if self.name == "pipe_valve3":
+						valve_anims.play("pipe_3_turn")
+						reactor_label.coolant += 2
+						coolantProvided += 2
 	if Global.current_object == "ValveBody4":
 		#Must correspond to the interactables display node in the current scene
 		interactables_display._show_interactable()
-		if Input.is_action_just_pressed("interact") or Input.is_action_just_pressed("left_click"):
-			if coolantProvided < maxCoolant:
-				if self.name == "pipe_valve4":
-					reactor_label.coolant += 20
-					coolantProvided += 20
+		if !valve_anims.is_playing():
+			if Input.is_action_just_pressed("interact") or Input.is_action_just_pressed("left_click"):
+				if coolantProvided < maxCoolant:
+					if self.name == "pipe_valve4":
+						valve_anims.play("pipe_4_turn")
+						reactor_label.coolant += 20
+						coolantProvided += 20
 
 func reset_pipe():
 	coolantProvided = 0

6f233a4170aff4c9bf8dfade1eba51715fe7563b
FDPcrewAJ <junga526@hsd.k12.or.us>
2025-04-20 19:03:01 -0700

    paritally implimented tile puzzle on first floor

---
+++ gd_root/Scripts/Rooms/RoomManagers/first_floor_interaction_control.gd
@@ -0,0 +1,24 @@
+extends Node3D
+
+# Player and other control
+@onready var player: Player = $"../player"
+@onready var interactables_display: Control = $"../InteractablesDisplay"
+
+# Boat Parts
+@onready var fuel_can: Node3D = $boat_parts/fuel_can
+@onready var fuel_can_box: Node3D = $boat_parts/fuel_can_box
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	if Global.current_object == "first_floor_tile_puzzle":
+		interactables_display._show_interactable()
+		if Input.is_action_just_pressed("interact") or Input.is_action_just_pressed("left_click"):
+			Global.lastpos = player.position
+			Global.previous_scene = "res://Scenes/Final Levels/first_floor.tscn"
+			get_tree().call_deferred("change_scene_to_file", "res://Scenes/Interfaces/tile_puzzle_interface.tscn")
+	
+	if Global.tile_puzzle_completed:
+		fuel_can.position = Vector3(-21, 0.5, 5)

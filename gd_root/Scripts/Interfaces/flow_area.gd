d1f9fb32ac83c1acc9c3954a5638e8c2171edbaa
Ainsworth_Adam <ainsa128@hsd.k12.or.us>
2025-04-23 15:43:07 -0700

    fixed the position of some scripts and some errors caused by the flow puzzle

--- gd_root/Scripts/Interfaces/flow_area.gd
+++ gd_root/Scripts/Interfaces/flow_area.gd
@@ -40,5 +40,5 @@ func _process(delta: float) -> void:
 			get_parent().active2 = false
 		if self.name == "Area2D3":
 			get_parent().active3 = false
-	if overlaps_area($"../../../StartLabel/StartArea2D") == true:
+	if overlaps_area($"../../../StartArea2D") == true:
 		get_parent().startFull = true

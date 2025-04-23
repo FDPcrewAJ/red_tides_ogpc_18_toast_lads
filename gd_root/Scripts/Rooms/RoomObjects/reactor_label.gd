b0ff0a36d16ea66b27a08f4a8330472f6bcd8b97
Ainsworth_Adam <ainsa128@hsd.k12.or.us>
2025-04-23 16:53:59 -0700

    updated reactor puzzle into water pressure puzzle. Also added invisible walls and boat part there and stuff

--- gd_root/Scripts/Rooms/RoomObjects/reactor_label.gd
+++ gd_root/Scripts/Rooms/RoomObjects/reactor_label.gd
@@ -4,7 +4,7 @@ var coolant = 0
 
 
 func _process(_delta: float) -> void:
-	text = "Coolant: " + str(coolant) + "/100"
+	text = "Pressure: " + str(coolant) + "/100"
 	if coolant == 100 or Global.reactorStable == true:
 		text = "Stabilized"
 		Global.reactorStable = true
---
+++ gd_root/Scripts/Rooms/RoomObjects/underwater_puzzle_interactions.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+		
+		# Motor
+	if Global.current_object == "motor_col":
+		$"../InteractablesDisplay"._show_interactable()
+		if Input.is_action_just_pressed("interact") or Input.is_action_just_pressed("left_click"):
+			$"../motor".position = Vector3(0, -10, 0)
+			Global.motor_collected = true

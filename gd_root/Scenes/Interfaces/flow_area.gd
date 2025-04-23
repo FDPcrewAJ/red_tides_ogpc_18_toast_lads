dd6c2cf54a8e26e0c7b9ef9c27da108dd42dbd3c
Ainsworth_Adam <ainsa128@hsd.k12.or.us>
2025-04-23 11:29:19 -0700

    finished flow puzzle

--- gd_root/Scenes/Interfaces/flow_area.gd
+++ gd_root/Scenes/Interfaces/flow_area.gd
@@ -29,6 +29,10 @@ func _process(delta: float) -> void:
 			if self.name == "Area2D3":
 				get_parent().active3 = true
 				get_parent().neighbor3 = get_overlapping_areas()[0].get_parent()
+		if get_overlapping_areas()[0].filled == true:
+			if self.name == "Area2D4":
+				get_parent().active4 = true
+				get_parent().neighbor3 = get_overlapping_areas()[0].get_parent()
 	else:
 		if self.name == "Area2D":
 			get_parent().active1 = false
--- gd_root/Scenes/Interfaces/pipe_button_base.gd
+++ gd_root/Scenes/Interfaces/pipe_button_base.gd
@@ -3,25 +3,28 @@ extends Button
 var active1 = false
 var active2 = false
 var active3 = false
+var active4 = false
 var full = false
 var startFull = false
 var neighbor1
 var neighbor2
 var neighbor3
 var reset = false
+var children = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 
 func reset_water():
-	get_child(0).reset()
-	get_child(1).reset()
-	get_child(2).reset()
+	children = get_child_count()
+	for n in children:
+		get_child(n).reset()
 	full = false
 	active1 = false
 	active2 = false
 	active3 = false
+	active4 = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -29,24 +32,22 @@ func _process(delta: float) -> void:
 	if startFull == true:
 		full = true
 		
-	if active1 == true or active2 == true or active3:
+	if active1 == true or active2 == true or active3 or active4 == true:
 		full = true
 	else:
 		full = false
 	
 	if full == true:
-		add_theme_color_override("font_disabled_color", Color(0, 1, 0, 1))
-		add_theme_color_override("font_color", Color(0, 1, 0, 1))
-		add_theme_color_override("font_outline_color", Color(0, 1, 0, 1))
-		add_theme_color_override("font_focus_color", Color(0, 1, 0, 1))
-		add_theme_color_override("font_hover_color", Color(0, 1, 0, 1))
-		add_theme_color_override("font_hover_pressed_color", Color(0, 1, 0, 1))
-		add_theme_color_override("font_pressed_color", Color(0, 1, 0, 1))
+		add_theme_color_override("icon_disabled_color", Color(0, 1, 0, 1))
+		add_theme_color_override("icon_focus_color", Color(0, 1, 0, 1))
+		add_theme_color_override("icon_hover_color", Color(0, 1, 0, 1))
+		add_theme_color_override("icon_hover_pressed_color", Color(0, 1, 0, 1))
+		add_theme_color_override("icon_normal_color", Color(0, 1, 0, 1))
+		add_theme_color_override("icon_pressed_color", Color(0, 1, 0, 1))
 	else:
-		add_theme_color_override("font_disabled_color", Color(1, 1, 1, 1))
-		add_theme_color_override("font_color", Color(1, 1, 1, 1))
-		add_theme_color_override("font_outline_color", Color(1, 1, 1, 1))
-		add_theme_color_override("font_focus_color", Color(1, 1, 1, 1))
-		add_theme_color_override("font_hover_color", Color(1, 1, 1, 1))
-		add_theme_color_override("font_hover_pressed_color", Color(1, 1, 1, 1))
-		add_theme_color_override("font_pressed_color", Color(1, 1, 1, 1))
+		add_theme_color_override("icon_disabled_color", Color(1, 1, 1, 1))
+		add_theme_color_override("icon_focus_color", Color(1, 1, 1, 1))
+		add_theme_color_override("icon_hover_color", Color(1, 1, 1, 1))
+		add_theme_color_override("icon_hover_pressed_color", Color(1, 1, 1, 1))
+		add_theme_color_override("icon_normal_color", Color(1, 1, 1, 1))
+		add_theme_color_override("icon_pressed_color", Color(1, 1, 1, 1))

cef6c7ea83449aa648926d9666d81851395aa3cb
TNTBoss971 <w.samuel.davidson@gmail.com>
2025-05-03 16:53:00 -0700

    fixed a wierd wall, and did some more lore

--- gd_root/Scripts/Rooms/RoomManagers/control_floor_puzzles.gd
+++ gd_root/Scripts/Rooms/RoomManagers/control_floor_puzzles.gd
@@ -22,6 +22,7 @@ func _process(_delta: float) -> void:
 			Global.previous_scene = "res://Scenes/Final Levels/control_floor.tscn"
 			get_tree().call_deferred("change_scene_to_file", "res://Scenes/Interfaces/dial_puzzle_interface.tscn")
 	
+	# chat logs
 	if Global.current_object == "lore_chatlog_01":
 		interactables_display._show_interactable()
 		if Input.is_action_just_pressed("interact") or Input.is_action_just_pressed("left_click"):
@@ -35,3 +36,18 @@ func _process(_delta: float) -> void:
 			Global.lastpos = player.position
 			Global.previous_scene = "res://Scenes/Final Levels/control_floor.tscn"
 			get_tree().call_deferred("change_scene_to_file", "res://Scenes/Interfaces/Lore/chatlog_interface_02.tscn")
+			
+	if Global.current_object == "lore_chatlog_03":
+		interactables_display._show_interactable()
+		if Input.is_action_just_pressed("interact") or Input.is_action_just_pressed("left_click"):
+			Global.lastpos = player.position
+			Global.previous_scene = "res://Scenes/Final Levels/control_floor.tscn"
+			get_tree().call_deferred("change_scene_to_file", "res://Scenes/Interfaces/Lore/chatlog_interface_03.tscn")
+			
+	# computer logs
+	if Global.current_object == "lore_logs_01":
+		interactables_display._show_interactable()
+		if Input.is_action_just_pressed("interact") or Input.is_action_just_pressed("left_click"):
+			Global.lastpos = player.position
+			Global.previous_scene = "res://Scenes/Final Levels/control_floor.tscn"
+			get_tree().call_deferred("change_scene_to_file", "res://Scenes/Interfaces/Lore/logs_interface_01.tscn")

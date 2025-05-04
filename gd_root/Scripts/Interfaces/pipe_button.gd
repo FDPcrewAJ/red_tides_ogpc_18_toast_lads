fa4c8bf39eee3ced5418f91517d24294135d0809
FDPcrewAJ <junga526@hsd.k12.or.us>
2025-05-04 11:52:11 -0700

    updated visuals of flow puzzle

--- gd_root/Scripts/Interfaces/pipe_button.gd
+++ gd_root/Scripts/Interfaces/pipe_button.gd
@@ -1,16 +1,6 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_pipe_button_pressed() -> void:
 	rotation_degrees += 90
 	$"..".reset_all_pipes()

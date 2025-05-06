e6437e42a6770c63f046970879852384b63d37cb
Ainsworth_Adam <ainsa128@hsd.k12.or.us>
2025-05-06 09:51:38 -0700

    fixed a  bug where the elevator label reset on re-entering the elevator

--- gd_root/Scripts/Rooms/RoomManagers/elevator_panel.gd
+++ gd_root/Scripts/Rooms/RoomManagers/elevator_panel.gd
@@ -11,13 +11,25 @@ extends Node3D
 @onready var floor_label: Label3D = $floor_feedback/floor_label
 
 func _process(_delta: float) -> void:
+	if Global.next_floor == "Water Processing":
+		floor_label.text = "Water Pumping"
+	if Global.next_floor == "Entrance":
+		floor_label.text = "Entrance"
+	if Global.next_floor == "First Floor":
+		floor_label.text = "Main Floor 1"
+	if Global.next_floor == "Second Floor":
+		floor_label.text = "Main Floor 2"
+	if Global.next_floor == "Control":
+		floor_label.text = "Control"
+	if Global.next_floor == "null":
+		floor_label.text = ")_%()*%#-0!$"
+	
 	# Floor 1
 	if Global.current_object == "button_1":
 		$"../../InteractablesDisplay"._show_interactable()
 		floor_1_label.modulate = Color(1, 0, 0)
 		if Input.is_action_just_pressed("interact") or Input.is_action_just_pressed("left_click"):
 			Global.next_floor = "Water Processing"
-			floor_label.text = "Water Pumping"
 	else:
 		floor_1_label.modulate = Color(0.54, 0, 0)
 	
@@ -27,7 +39,6 @@ func _process(_delta: float) -> void:
 		floor_2_label.modulate = Color(1, 0, 0)
 		if Input.is_action_just_pressed("interact") or Input.is_action_just_pressed("left_click"):
 			Global.next_floor = "Entrance"
-			floor_label.text = "Entrance"
 	else:
 		floor_2_label.modulate = Color(0.54, 0, 0)
 	
@@ -37,7 +48,6 @@ func _process(_delta: float) -> void:
 		floor_3_label.modulate = Color(1, 0, 0)
 		if Input.is_action_just_pressed("interact") or Input.is_action_just_pressed("left_click"):
 			Global.next_floor = "First Floor"
-			floor_label.text = "Main Floor 1"
 	else:
 		floor_3_label.modulate = Color(0.54, 0, 0)
 	
@@ -47,7 +57,6 @@ func _process(_delta: float) -> void:
 		floor_4_label.modulate = Color(1, 0, 0)
 		if Input.is_action_just_pressed("interact") or Input.is_action_just_pressed("left_click"):
 			Global.next_floor = "Second Floor"
-			floor_label.text = "Main Floor 2"
 	else:
 		floor_4_label.modulate = Color(0.54, 0, 0)
 	
@@ -57,6 +66,5 @@ func _process(_delta: float) -> void:
 		floor_5_label.modulate = Color(1, 0, 0)
 		if Input.is_action_just_pressed("interact") or Input.is_action_just_pressed("left_click"):
 			Global.next_floor = "Control"
-			floor_label.text = "Control"
 	else:
 			floor_5_label.modulate = Color(0.54, 0, 0)

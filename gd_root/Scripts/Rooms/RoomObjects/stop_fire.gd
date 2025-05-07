7f861cb28a1aa0b0686e97f3cfc2a5713684f5e1
Jungels_Aiden <junga526@hsd.k12.or.us>
2025-05-07 13:26:00 -0700

    updated fog on fire puzzle

--- gd_root/Scripts/Rooms/RoomObjects/stop_fire.gd
+++ gd_root/Scripts/Rooms/RoomObjects/stop_fire.gd
@@ -7,6 +7,7 @@ signal fire_stopped
 @onready var fire_console_2: CSGMesh3D = $"../hub_room/FireConsole2"
 @onready var fire_console_3: CSGMesh3D = $"../hub_room/FireConsole3"
 @onready var lights: Node3D = $"../lights"
+@onready var world_env: WorldEnvironment = $"../WorldEnv"
 
 
 func _process(_delta: float) -> void:
@@ -15,6 +16,7 @@ func _process(_delta: float) -> void:
 			#Must correspond to the interactables display node in the current scene
 			interactables_display._show_interactable()
 			if Input.is_action_just_pressed("interact") or Input.is_action_just_pressed("left_click"):
+				world_env.environment.fog_enabled = false
 				Global.fire_puzzle_completed = true
 				Global.tutorial_completed = true
 				self.rotation.z = deg_to_rad(-180)

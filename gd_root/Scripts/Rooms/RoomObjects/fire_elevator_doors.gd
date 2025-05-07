bb7b3c045733a1641746aa3c8c56366ad5b867cc
FDPcrewAJ <junga526@hsd.k12.or.us>
2025-05-07 15:04:20 -0700

    player now gets sent to the correct spot in the control floor after completing the fire puzzle

--- gd_root/Scripts/Rooms/RoomObjects/fire_elevator_doors.gd
+++ gd_root/Scripts/Rooms/RoomObjects/fire_elevator_doors.gd
@@ -7,5 +7,5 @@ func _process(_delta):
 			#Must correspond to the interactables display node in the current scene
 			$"../InteractablesDisplay"._show_interactable()
 			if Input.is_action_just_pressed("interact") or Input.is_action_just_pressed("left_click"):
-				Global.lastpos = 0
+				Global.lastpos = Vector3(-6.75, 1.6, 10.7)
 				get_tree().call_deferred("change_scene_to_file", "res://Scenes/Final Levels/control_floor.tscn")

b014e7e3ced10692a8cdd75a703112c835db99fb
FDPcrewAJ <junga526@hsd.k12.or.us>
2025-05-09 00:26:53 -0700

    finished the death cutscene

--- gd_root/Scripts/Menus/death.gd
+++ gd_root/Scripts/Menus/death.gd
@@ -1,15 +1,36 @@
-extends Node2D
+extends Control
+
+@onready var timer: Timer = $"ending UI/Timer"
+@onready var text_1: Label = $"ending UI/text_1"
+@onready var text_2: Label = $"ending UI/text_2"
+@onready var main_menu_button: Button = $"ending UI/main_menu_button"
+@onready var death_video: VideoStreamPlayer = $death_video
 
 
-# Called when the node enters the scene tree for the first time.
+var time_num = 0
+
 func _ready() -> void:
-	pass # Replace with function body.
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	Global.music_control.playing = false
+
+func _on_death_video_finished() -> void:
+	timer.start()
+	death_video.queue_free()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_timer_timeout() -> void:
+	match time_num:
+		0:
+			pass
+		1:
+			text_1.show()
+		2:
+			text_2.show()
+		3:
+			main_menu_button.show()
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	time_num += 1
 
 
-func _on_button_pressed() -> void:
-	get_tree().call_deferred("change_scene_to_file", "res://Scenes/Menus/title_screen.tscn")
+func _on_main_menu_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Menus/title_screen.tscn")
